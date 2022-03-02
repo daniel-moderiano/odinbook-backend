318d56eebd2b708eb65b7584b7b95d162b636ef9
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-02 21:41:12 +1030

    Protect all comment routes

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -10,8 +10,8 @@ router.route('/:postId').get(getPost).put(updatePost).delete(deletePost);
 router.route('/:postId/likes').put(likePost);
 
 // Comment-specific routes (using the post routes as a base route)
-router.route('/:postId/comments').get(getComments).post(protectRoute, addComment);
-router.route('/:postId/comments/:commentId').get(getComment).put(updateComment).delete(deleteComment);
+router.route('/:postId/comments').get(protectRoute, getComments).post(protectRoute, addComment);
+router.route('/:postId/comments/:commentId').get(protectRoute, getComment).put(protectRoute, updateComment).delete(protectRoute, deleteComment);
 router.route('/:postId/comments/:commentId/likes').put(protectRoute, likeComment);
 
 module.exports = router;
