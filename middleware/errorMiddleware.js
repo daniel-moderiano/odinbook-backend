2511573965651666b4a8b98929afac5b16180410
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-02 11:00:27 +1030

    Add error handler middleware with unit testing

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,16 @@
+// Replace the inbuilt express error handler by defining a middleware func that accepts the err object in addition to the usual middleware params
+const errorHandler = (err, req, res, next) => {
+  // Check if a status code was manually set already during the req/res cycle, otherwise use default 500 internal server error
+  console.log(res.statusCode);
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  // Return JSON instead of the default HTML error template by Express
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? undefined : err.stack // Do not add stack for production apps
+  });
+};
+
+module.exports = {
+  errorHandler,
+}
