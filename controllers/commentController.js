1d696eed8dfb2a8120876c507b91db42829bbf6c
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-02 19:05:05 +1030

    Implement logic for addComment and updateComment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler');
 const Post = require('../models/PostModel');
 const User = require('../models/UserModel');
 const Comment = require('../models/CommentModel');
+const { body, validationResult } = require("express-validator");
 
 // @desc    Get all comments
 // @route   GET /api/posts/:postId/comments
@@ -30,16 +31,69 @@ const getComment = asyncHandler(async (req, res) => {
 // @desc    Add new comment
 // @route   POST /api/posts/:postId/comments/
 // @access  Private
-const addComment = asyncHandler(async (req, res) => {
-  res.status(200).json({ comment: 'Comment data' })
-});
+const addComment = [
+  // Validate text input. No sanitisation taking place here; this data is not used to execute any commands. Take care to sanitise as needed on frontend output/use
+  body('text', 'Comment text is required').trim().isLength({ min: 1 }),
+
+  // Process request after input data has been validated
+  asyncHandler(async (req, res, next) => {
+
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // Create new comment
+    const newComment = new Comment({
+      user: req.user._id,   // requires currently logged in user, which will be in the request body
+      text: req.body.text, 
+      likes: [],
+    });
+
+    // Validation errors have occurred. Return these to the user is JSON format
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors.array());   // Do not throw error here, allow frontend to handle as needed
+    } else {
+      // Form data is valid. Save to db
+      await newComment.save();
+      res.status(200).json(newComment)   // Return status OK and new comment to client
+    }
+  }),
+];
 
 // @desc    Update single post
 // @route   PUT /api/posts/:postId/comments/:commentId
 // @access  Private
-const updateComment = asyncHandler(async (req, res) => {
-  res.status(200).json({ comment: 'Comment data' })
-});
+const updateComment = [
+  // Validate data
+  body('text', 'Comment text is required').trim().isLength({ min: 1 }),
+
+  // Process request after input data has been validated
+  asyncHandler(async (req, res, next) => {
+
+    // Extract the validation errors from a request
+    const errors = validationResult(req);
+
+    // Validation errors have occurred. Return these to the user
+    if (!errors.isEmpty()) {
+      res.status(400).json(errors.array());   // Do not throw single error here, need to pass all errors to user instead
+    } else {
+      // Submitted data is valid
+      // Check if comment exists in db
+      const comment = await Comment.findById(req.params.commentId);
+
+      if (!comment) {  // comment not found in db
+        res.status(400);
+        throw new Error('Comment not found');
+      }
+
+      // Update comment in db
+      const updatedComment = await Comment.findByIdAndUpdate(req.params.commentId, {
+        text: req.body.text,
+      }, { new: true });  // { new: true } ensures the updated comment is returned
+
+      res.status(200).json(updatedComment);   // Return status OK and updated comment to client
+    }
+  }),
+];
 
 // @desc    Like a single post (i.e. add new user to likes array)
 // @route   PUT /api/posts/:postId/comments/:commentId/likes
