4dc911d7cb92fa363cb16e5518fc758bfea60ca5
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-03-05 20:23:47 +1030

    Write tests for modifyFriendArray functions

--- __tests__/friendController.test.js
+++ __tests__/friendController.test.js
@@ -1,63 +1,111 @@
-const { checkExistingEntries } = require('../controllers/friendController');
+const { checkExistingEntries, modifyForAcceptRequest, modifyForCancelRequest, modifyForDeleteRequest, modifyForSendRequest } = require('../controllers/friendController');
 
 const sender = {
   id: 'ron',
-  friends: [
-    { user: 'hermione', status: 'friend' },
-    { user: 'ginny', status: 'outgoing' },
-    { user: 'luna', status: 'incoming' },
-    { user: 'neville', status: 'deleted' }
-  ],
+  friends: [],
 };
-const recipeint = {
+const recipient = {
   id: 'harry',
-  friends: [
-    { user: 'hermione', status: 'friend' },
-    { user: 'ginny', status: 'outgoing' },
-    { user: 'luna', status: 'incoming' },
-    { user: 'neville', status: 'deleted' }
-  ],
+  friends: []
 };
 
-// Reset both arrays before each test to avoid accumulating requests
-beforeEach(() => {
-  sender.friends = [
-    { user: 'hermione', status: 'friend' },
-    { user: 'ginny', status: 'outgoing' },
-    { user: 'luna', status: 'incoming' },
-    { user: 'neville', status: 'deleted' }
-  ];
-  recipeint.friends = [
-    { user: 'hermione', status: 'friend' },
-    { user: 'ginny', status: 'outgoing' },
-    { user: 'luna', status: 'incoming' },
-    { user: 'neville', status: 'deleted' }
-  ];
-})
 
+// Push purposely clashing entires into friends array before running tests
 describe("checkExistingEntries function can identify existing entires of all sorts in a user's friends array", () => {
+  // Reset arrays prior to each test
+  beforeEach(() => {
+    sender.friends = [
+      { user: 'hermione', status: 'friend' },
+      { user: 'ginny', status: 'outgoing' },
+      { user: 'luna', status: 'incoming' },
+      { user: 'neville', status: 'deleted' }
+    ];
+  })
+
   test('Return null when no clashing entires exist', () => {
-    expect(checkExistingEntries(recipeint.id, sender.friends)).toBe(null);
+    expect(checkExistingEntries(recipient.id, sender.friends)).toBe(null);
   });
 
   test("Return 'incoming' when the recipient has already sent the sender a request", () => {
     sender.friends.push({ user: 'harry', status: 'incomingRequest' });
-    expect(checkExistingEntries(recipeint.id, sender.friends)).toBe('incomingRequest');
+    expect(checkExistingEntries(recipient.id, sender.friends)).toBe('incomingRequest');
   });
 
   test("Return 'outgoing' when the sender has already sent the recipient a request", () => {
     sender.friends.push({ user: 'harry', status: 'outgoingRequest' });
-    expect(checkExistingEntries(recipeint.id, sender.friends)).toBe('outgoingRequest');
+    expect(checkExistingEntries(recipient.id, sender.friends)).toBe('outgoingRequest');
   });
 
   test("Return 'deleted' when the recipient has deleted a previous request from the sender", () => {
     sender.friends.push({ user: 'harry', status: 'deletedRequest' });
-    expect(checkExistingEntries(recipeint.id, sender.friends)).toBe('deletedRequest');
+    expect(checkExistingEntries(recipient.id, sender.friends)).toBe('deletedRequest');
   });
 
   test("Return 'friend' when the recipient and sender are already friends", () => {
     sender.friends.push({ user: 'harry', status: 'friend' });
-    expect(checkExistingEntries(recipeint.id, sender.friends)).toBe('friend');
+    expect(checkExistingEntries(recipient.id, sender.friends)).toBe('friend');
+  });
+});
+
+
+// Push purposely clashing entires into friends array before running tests
+describe("Modification functions make the correct adjustments to sender and recipient friend arrays", () => {
+  // Reset arrays prior to each test
+  beforeEach(() => {
+    sender.friends = [];
+    recipient.friends = [];
+  });
+
+  test('Accepting a request modifies sender array accordingly', () => {
+    sender.friends = [{ user: 'ron', status: 'outgoingRequest' }];
+    recipient.friends = [{ user: 'harry', status: 'incomingRequest' }];
+    modifyForAcceptRequest(sender, recipient);
+    expect(sender.friends[0].status).toBe('friend');
+  });
+
+  test('Accepting a request modifies recipient array accordingly', () => {
+    sender.friends = [{ user: 'ron', status: 'outgoingRequest' }];
+    recipient.friends = [{ user: 'harry', status: 'incomingRequest' }];
+    modifyForAcceptRequest(sender, recipient);
+    expect(recipient.friends[0].status).toBe('friend');
+  });
+
+  test("Deleting a request modifies sender array accordingly", () => {
+    sender.friends = [{ user: 'ron', status: 'incomingRequest' }];
+    recipient.friends = [{ user: 'harry', status: 'outgoingRequest' }];
+    modifyForDeleteRequest(sender, recipient);
+    expect(sender.friends.length).toBe(0);
+  });
+  
+  test("Deleting a request modifies recipient array accordingly", () => {
+    sender.friends = [{ user: 'ron', status: 'incomingRequest' }];
+    recipient.friends = [{ user: 'harry', status: 'outgoingRequest' }];
+    modifyForDeleteRequest(sender, recipient);
+    expect(recipient.friends[0].status).toBe('deletedRequest');
+  });
+
+  test("Cancelling a request modifies sender array accordingly", () => {
+    sender.friends = [{ user: 'ron', status: 'outgoingRequest' }];
+    recipient.friends = [{ user: 'harry', status: 'incomingRequest' }];
+    modifyForCancelRequest(sender, recipient);
+    expect(sender.friends.length).toBe(0);
+  });
+
+  test("Cancelling a request modifies recipient array accordingly", () => {
+    sender.friends = [{ user: 'ron', status: 'outgoingRequest' }];
+    recipient.friends = [{ user: 'harry', status: 'incomingRequest' }];
+    modifyForCancelRequest(sender, recipient);
+    expect(recipient.friends.length).toBe(0);
+  });
+
+  test("Sending a request modifies sender array accordingly", () => {
+    modifyForSendRequest(sender, recipient);
+    expect(sender.friends[0].status).toBe('outgoingRequest');
+  });
+
+  test("Sending a request modifies recipient array accordingly", () => {
+    modifyForSendRequest(sender, recipient);
+    expect(recipient.friends[0].status).toBe('incomingRequest');
   });
 });
 
--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,94 +1,41 @@
-// Example user models
-const ron = {
-  _id: '1234',
-  name: 'ron',
-  friends: []
-}
-
-const harry = {
-  _id: '5678',
-  name: 'peter',
-  friends: []
-}
-
-
-// Returns true where there is already an outgoing request to the user
-const outgoingRequestExists = (userId, friendsArray) => {
-  return friendsArray.some((request) => (
-    request.status === 'outgoingRequest' && request.user === userId
-  ));
-};
-
-// Returns true where there is already an incoming request from the user
-const incomingRequestExists = (userId, friendsArray) => {
-  return friendsArray.some((request) => (
-    request.status === 'incomingRequest' && request.user === userId
-  ));
-};
-
-// Returns true where there is already an deleted request from the user
-const deletedRequestExists = (userId, friendsArray) => {
-  return friendsArray.some((request) => (
-    request.status === 'deleted' && request.user === userId
-  ));
-};
-
-// Consider abstracting the different logic in the hadnle func below, e.g. func for send, accept, delete, and cancel request
-
-// Both users are initialised with empty friend arrays. Friend request operations and desired outcomes will be brainstormed below.
-
-// const sendFriendRequest = (sender, recipient) => {
-//   // Check all relevant cases where a new request cannot be sent
-//   if (outgoingRequestExists(recipient._id, sender.friends)) {
-//     res.status(400);
-//     throw new Error('Request has already been sent to this user');
-//   }
-
-//   if (incomingRequestExists(recipient._id, sender.friends)) {
-//     res.status(400);
-//     throw new Error('Request already exists from this user');
-//   }
-
-//   if (deletedRequestExists(recipient._id, sender.friends)) {
-//     res.status(400);
-//     throw new Error('User has denied your request');
-//   }
-
-//   // Request able to be sent. Adjust receipeint and sender's friends as needed
-//   recipient.friends.push({   // add incoming request to recipient doc
-//     user: req.user._id,
-//     status: 'incomingRequest'
-//   });
-
-//   sender.friends.push({   // add outgoing request to sender doc
-//     user: req.params.userId,
-//     status: 'outgoingRequest'
-//   });
-
-//   return 
-// }
-
+// Use this function to ensure that no duplicate requests are sent, and that certain request types exist before performing related actions  
 const checkExistingEntries = (userId, friendsArray) => {
-  // Identify existing request for the user in the friendsArray provided. There should never be more than one existing
+  // Identify existing request for the user in the friendsArray provided. There should never be more than one existing entry
   const existingRequests = friendsArray.filter((request) => request.user === userId);
-
-  console.log(friendsArray);
 
   if (existingRequests.length === 0) {  // No request exists, exit function here
     return null;
   } else {  // Existing request is present. Return type of request.
     return existingRequests[0].status;
   }
-}
+};
+
+const modifyForSendRequest = (sender, recipeint) => {
+
+};
+
+const modifyForAcceptRequest = (sender, recipeint) => {
+
+};
+
+const modifyForCancelRequest = (sender, recipeint) => {
+
+};
+
+const modifyForDeleteRequest = (sender, recipeint) => {
+
+};
+
 
 // ! PUT can be done using any data you manually enter in the request body from the front end! Can specify type of update there!
 // Expected API route would be PUT api/friends/:userId
 const handleFriendRequest = async (req, res) => {
   // Req.user will contain the sender's details
-  // When clicking on another user's details, their ID should be captured and passed into the req.params.friendId for example
+  // When clicking on another user's details, their ID should be captured and passed into the req.params.userId for example
   // In all friend request functions, the requestSender describes the currently logged in user that is making a request of some kind, be it sending, accepting, cancelling, or deleting. The recepient describes the user that is the target of the sender's request, again regardless of if the sender if sending a request to that user, or accepting a request from that user
   const requestRecipient = await User.findById(req.params.userId, 'firstName lastName profilePic friends');
   const requestSender = await User.findById(req.user._id, 'firstName lastName profilePic friends');
+
   if (!requestRecipient) {  // user not found in db
     res.status(400);
     throw new Error('User not found');
@@ -97,34 +44,23 @@ const handleFriendRequest = async (req, res) => {
   // Incoming req.body will contain the type of operation required. Perform logic as needed
   switch (req.body.requestType) {
     case 'sendRequest':   // user is sending a friend request to another user
-      // Check all relevant cases where a new request cannot be sent
-      if (outgoingRequestExists(req.params.userId, requestSender.friends)) {
-        res.status(400);
-        throw new Error('Request has already been sent to this user');
+      const existingRequest = checkExistingEntries(requestRecipient._id, requestSender.friends);
+      if (!existingRequest) {
+        // Request able to be sent. Adjust receipeint and sender's friends as needed
+        requestRecipient.friends.push({   // add incoming request to recipient doc
+          user: req.user._id,
+          status: 'incomingRequest'
+        });
+
+        requestSender.friends.push({   // add outgoing request to sender doc
+          user: req.params.userId,
+          status: 'outgoingRequest'
+        });
+        break;
+      } else {
+        // Request cannot be sent. Customise error message depending on reason
       }
 
-      if (incomingRequestExists(req.params.userId, requestSender.friends)) {
-        res.status(400);
-        throw new Error('Request already exists from this user');
-      }
-
-      if (deletedRequestExists(req.params.userId, requestSender.friends)) {
-        res.status(400);
-        throw new Error('User has denied your request');
-      }
-
-      // ! Check if freind exists!! 
-
-      // Request able to be sent. Adjust receipeint and sender's friends as needed
-      requestRecipient.friends.push({   // add incoming request to recipient doc
-        user: req.user._id,
-        status: 'incomingRequest'
-      });
-
-      requestSender.friends.push({   // add outgoing request to sender doc
-        user: req.params.userId,
-        status: 'outgoingRequest'
-      });
       break;
 
     case 'acceptRequest':   // user is accepting a friend request from another user
@@ -167,65 +103,10 @@ const handleFriendRequest = async (req, res) => {
   res.json(updatedRecipient);
 };
 
-
-// SCENARIO: Harry accepts Ron's friend request
-// User models will need to end up as
-ron.friends = [{ user: '5678', status: 'friend' }];
-harry.friends = [{ user: '1234', status: 'friend' }];
-
-// Expected API route would be PUT api/friends/:friendId
-const sendFriendRequest = async (req, res) => {
-  // Req.user will contain Ron's details
-  // When clicking on another user's details, their ID should be captured and passed into the req.params.friendId for example
-  const requestRecipient = await User.findById(req.params.friendId, 'firstName lastName profilePic friends');
-  const requestSender = await User.findById(req.user._id, 'firstName lastName profilePic friends');
-  if (!requestRecipient) {  // user not found in db
-    res.status(400);
-    throw new Error('User not found');
-  } 
-
-  // Request recipient is valid, amend both sender and receiver arrays accordingly
-  requestRecipient.friends.push({   // add incoming request to recipient doc
-    user: req.user._id,
-    status: 'incomingRequest'
-  });
-
-  requestSender.friends.push({   // add outgoing request to sender doc
-    user: req.params.friendId,
-    status: 'outgoingRequest'
-  });
-
-  // Save these changes to the db
-  await requestRecipient.save();
-  await requestSender.save();
-
-  // Return information of recipient (to populate a 'request sent to: ' messaged in frontend. Check that password is not being sent here though!)
-  res.json(updatedRecipient);
-};
-
-
-
-
-// Ron send request to harry
-ron.friends = [{ user: '5678', status: 'outgoingRequest' }]
-harry.friends = [{ user: '1234', status: 'incomingRequest' }]
-
-// Ron attempts to resend a friend request to Harry
-// Must search Ron's friends array for an entry with Harry's user ID, then review whether this is outgoing, incoming, or deleted. This should be adjusted with frontend rendering to disable duplicate requests anyway, but backend should protect against it either way to be sure
-
-
-// Harry accepts request
-ron.friends = [{ user: '5678', status: 'friend' }]
-harry.friends = [{ user: '1234', status: 'friend' }]
-
-const test = [
-    { user: '5678', status: 'friend' },
-    { user: '1234', status: 'outgoingRequest' },
-    { user: '3456', status: 'outgoingRequest' },
-    { user: '1277', status: 'outgoingRequest' },
-    { user: '1234', status: 'incomingRequest' },
-]
-
 module.exports = {
   checkExistingEntries,
+  modifyForAcceptRequest,
+  modifyForCancelRequest,
+  modifyForSendRequest,
+  modifyForDeleteRequest,
 }
