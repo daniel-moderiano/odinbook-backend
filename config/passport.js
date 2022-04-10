0d8a1dce9231eb64efeab1baae5e369427a9b8a7
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-10 21:54:33 +0930

    Adjust serializeUser and deserializeUser functions to use _id instead of entire user object for better safety

--- config/passport.js
+++ config/passport.js
@@ -40,8 +40,11 @@ passport.use(strategy);
 
 // Required functions for the passport middleware. No customaisation required
 passport.serializeUser(function(user, done) {
-  done(null, user);
+  done(null, user.id);
 });
-passport.deserializeUser(function(user, done) {
-  done(null, user);
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
 });
