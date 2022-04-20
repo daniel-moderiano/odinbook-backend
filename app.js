d95155507806874dd8e72168ab3deea2af1311db
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-20 15:30:35 +0930

    Adjust back to production mode

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(session({
     sameSite: "none",
     httpOnly: true,
     secure: true,
-    maxAge: 1000 * 60 * 60 * 24 * 30,
+    maxAge: 1000 * 60 * 60 * 24 * 7,    // 7 days
   },
 }));
 
