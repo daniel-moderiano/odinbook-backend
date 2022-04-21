8fc06345486c45c317fa3803a8e2ac2a0ec9cec2
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-21 10:57:23 +0930

    Switch to dev mode

--- config/passportFB.js
+++ config/passportFB.js
@@ -9,13 +9,14 @@ const strategyOptions = {
   clientID: process.env['FACEBOOK_APP_ID'],
   clientSecret: process.env['FACEBOOK_APP_SECRET'],
   // When the user is successfully authenticated via the FB login, they will be redirected to this IRL
-  callbackURL: `${process.env.BACKEND_URL}auth/facebook/callback`,
+  callbackURL: `${process.env.FRONTEND_URL}/auth/facebook/callback`,
   profileFields: ['id', 'name', 'email', 'picture.type(large)']
 }
 
 const verifyCallback = asyncHandler(async (accessToken, refreshToken, profile, done) => {
   // Check for existing user in db
   const user = await User.findOne({ facebookId: profile.id });
+  // TODO: COnsider Azure image analysis here?
 
   if (user) {   // user has logged into this app with FB before. Link with their account and log in
     return done(null, user);
