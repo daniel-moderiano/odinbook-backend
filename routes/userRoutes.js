48b4abdbeeeb103bf74ddd7db2d3c7c9bd87c11a
Daniel Moderiano <daniel.moderiano@outlook.com>
2022-04-24 11:15:55 +0930

    Remove deleteUser route and controller as this is never used

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,7 +9,9 @@ router.post('/login/test', loginTestUser);
 router.post('/logout', logoutUser);
 router.post('/register', registerUser);
 router.get('/current', protectRoute, getCurrentUser);
-router.route('/:userId').get(protectRoute, getUser).delete(protectRoute, deleteUser).put(protectRoute, updateUser);
+router.route('/:userId')
+  .get(protectRoute, getUser)
+  .put(protectRoute, updateUser);
 router.route('/:userId/posts').get(protectRoute, getUserPosts);
 router.route('/:userId/feed').get(protectRoute, getUserFeed);
 router.route('/:userId/friends').get(protectRoute, getUserFriends);
